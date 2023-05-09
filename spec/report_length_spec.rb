fc22497ae41b6c421249ed6a4de649a8ff861cb9
Sean Peters <sp@snptrs.com>
2023-05-09 17:10:41 +0100

    Adds report_length tests

--- spec/report_length_spec.rb
+++ spec/report_length_spec.rb
@@ -1,8 +1,13 @@
 require 'report_length'
 
 RSpec.describe "report_length method" do
-  it "Returns 'Hello, Sean!'" do
-    result = greet("Sean")
-    expect(result).to eq "Hello, Sean!"
+  it "Returns 3 when given 'Dog'" do
+    expect(report_length('Dog')).to eq 'This string was 3 characters long.'
+  end
+  it "Returns 5 when given 'Table'" do
+    expect(report_length('Table')).to eq 'This string was 5 characters long.'
+  end
+  it "Returns 7 when given 'Giraffe'" do
+    expect(report_length('Giraffe')).to eq 'This string was 7 characters long.'
   end
 end
